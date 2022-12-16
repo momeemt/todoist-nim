13db2154b8a0ee23e4a977e416d32b72b09216b6
Mutsuha Asada <momiimt@gmail.com>
2022-12-16 22:41:06 +0900

    👍 add project

--- src/todoist.nim
+++ src/todoist.nim
@@ -1,35 +1,74 @@
 import std/json
 import std/options
 import std/strformat
+import std/strutils
 import std/httpclient
 
 import jsony
 
 type
   Project* = object
-    id: string
-    parentId: string
-    order: int
-    color: string
+    id: Option[string]
+    parentId: Option[string]
+    order: Option[int]
+    color: Color
     name: string
-    commentCount: int
-    isShared: bool
+    commentCount: Option[int]
+    isShared: Option[bool]
     isFavorite: bool
-    isInboxProject: bool
-    isTeamInbox: bool
-    url: string
-    viewStyle: string
+    isInboxProject: Option[bool]
+    isTeamInbox: Option[bool]
+    url: Option[string]
+    viewStyle: Option[string]
+  
+  Color* {.pure.} = enum
+    BerryRed = "berry_red"
+    Red = "red"
+    Orange = "orange"
+    Yellow = "yellow"
+    OliveGreen = "olive_green"
+    LimeGreen = "lime_green"
+    Green = "green"
+    MintGreen = "mint_green"
+    Teal = "teal"
+    SkyBlue = "sky_blue"
+    LightBlue = "light_blue"
+    Blue = "blue"
+    Grape = "grape"
+    Violet = "violet"
+    Lavender = "lavender"
+    Magenta = "magenta"
+    Salmon = "salmon"
+    Charcoal = "charcoal"
+    Grey = "grey"
+    Taupe = "taupe"
 
 const
   RestBaseUrl* = "https://api.todoist.com/rest/v2"
+
+proc enumHook* (v: string): Color =
+  result = parseEnum[Color](v)
 
 proc todoist* (token: string): HttpClient =
   result = newHttpClient()
   result.headers["Authorization"] = &"Bearer {token}"
 
-proc projects* (client: var HttpClient): seq[Project] =
+func initProject* (color: Color, name: string, isFavorite: bool = false): Project =
+  result = Project(
+    color: color,
+    name: name,
+    isFavorite: isFavorite,
+  )
+
+proc projects* (client: HttpClient): seq[Project] =
   let
     response = client.request(RestBaseUrl & "/projects", HttpGet)
     body = response.body.parseJson
   for elem in body:
     result.add ($elem).fromJson(Project)
+
+proc addProject* (client: HttpClient, project: Project): Project =
+  var client = client
+  client.headers["Content-Type"] = "application/json"
+  let response = client.request(RestBaseUrl & "/projects", HttpPost, project.toJson)
+  result = response.body.fromJson(Project)
--- tests/test_main.nim
+++ tests/test_main.nim
@@ -4,7 +4,17 @@ import std/unittest
 import todoist
 import dotenv
 
-test "projects":
-  load()
-  var todoist = todoist(getEnv("Authorization"))
-  echo todoist.projects()
+load()
+
+# test "projects":
+#   var todoist = todoist(getEnv("Authorization"))
+#   echo todoist.projects()
+
+# test "add project":
+#   var todoist = todoist(getEnv("Authorization"))
+#   echo todoist.addProject("Test Project")
+
+test "add project":
+  var client = todoist(getEnv("Authorization"))
+  let project = initProject(todoist.Color.MintGreen, "myProject4")
+  echo client.addProject(project)
